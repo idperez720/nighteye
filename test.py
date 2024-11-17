1f49f4206d2505c4d46a2d5dd8fb1e8c91ebc815
idperez720 <ivandavidperez4@gmail.com>
2024-11-17 11:21:03 -0500

    tests local

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 """Run test cases for the resource usage detection module."""
 
 from tests.resource_usage import run_detection_tests
-from tests.resource_usage_joint import run_detection_tests as run_detection_tests_joint
+# from tests.resource_usage_joint import run_detection_tests as run_detection_tests_joint
 
 if __name__ == "__main__":
-    run_detection_tests_joint()
+    run_detection_tests()
