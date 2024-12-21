8af73a1ea27da942faf827bbc726acd6660b2ce9
idperez720 <ivandavidperez4@gmail.com>
2024-12-21 17:59:00 -0500

    added test delegation

--- test.py
+++ test.py
@@ -2,8 +2,8 @@
 
 from tests.resource_usage import run_detection_tests
 from tests.resource_usage_server import run_detection_tests as run_detection_tests_server
-# from tests.resource_usage_joint import run_detection_tests as run_detection_tests_joint
+from tests.resource_usage_delegation import run_detection_tests as run_detection_tests_delegation
 
 if __name__ == "__main__":
     # run_detection_tests()
-    run_detection_tests_server(server_ip='172.20.10.8')
+    run_detection_tests_delegation(server_ip='172.20.10.8')
