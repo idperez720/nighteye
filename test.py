d1a50551287a0a2dfa646f45e78b6489d1f47012
= <=>
2025-03-03 13:56:55 -0500

    Parametrize tests

--- test.py
+++ test.py
@@ -1,9 +1,38 @@
 """Run test cases for the resource usage detection module."""
 
+import argparse
 from tests.resource_usage import run_detection_tests
-from tests.resource_usage_server import run_detection_tests as run_detection_tests_server
-from tests.resource_usage_delegation import run_detection_tests as run_detection_tests_delegation
+from tests.resource_usage_server import (
+    run_detection_tests as run_detection_tests_server,
+)
+from tests.resource_usage_delegation import (
+    run_detection_tests as run_detection_tests_delegation,
+)
 
 if __name__ == "__main__":
-    # run_detection_tests()
-    run_detection_tests_delegation(server_ip='172.20.10.8')
+    parser = argparse.ArgumentParser(description="Captura y procesa imágenes.")
+    parser.add_argument(
+        "--type_inference",
+        type=str,
+        default="local",
+        help="Realizar inferencia en el servidor",
+    )
+    parser.add_argument(
+        "--server_ip",
+        type=str,
+        default=None,
+        help="Realizar inferencia en el servidor",
+    )
+    args = parser.parse_args()
+
+    if args.type_inference == "local":
+        print("Tests local")
+        run_detection_tests()
+    elif args.type_inference == "server":
+        print("Tests en el servidor")
+        run_detection_tests_server(server_ip=args.server_ip)
+    elif args.type_inference == "delegation":
+        print("Tests Task Delegation en conjunta")
+        run_detection_tests_delegation(server_ip=args.server_ip)
+    else:
+        print("Tipo de inferencia no válido: local, server, joint")
